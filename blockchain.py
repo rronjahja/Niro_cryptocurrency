fa1ac41e3d6c44d481b3c330d59891507d76baf4
Rron Jahja <rronjahja@gmail.com>
2020-11-22 23:27:13 +0100

    Added Hash sha256

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,7 @@
 # Initializing our (empty) blockchain list
-import functools
+from functools import reduce
+import hashlib as hl
+import json
 
 MINING_REWARD = 10
 genesis_block = {
@@ -14,18 +16,19 @@ participants = {'Rron'}
 
 
 def hash_block(block):
-    return '-'.join([str(block[key]) for key in block])
+    return hl.sha256(json.dumps(block).encode()).hexdigest()
 
 
 def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum+0, tx_sender, 0)
+    amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,
+                                   tx_sender, 0)
 
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in
                     blockchain]
-    amount_recieved = functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)  if len(tx_amt) > 0 else tx_sum+0,
+    amount_recieved = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,
                                        tx_recipient, 0)
 
     return amount_recieved - amount_sent
@@ -67,6 +70,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
+    print(hashed_block)
     reward_transaction = {
         'sender': 'MINING',
         'recipient': owner,
